240ef49b70e22a4b8c8ae0189d70d06e48c945f6
yangyao <873241789@qq.com>
2020-04-26 12:08:47 +0800

    修改dockerfile文件名

--- src/container/Course/CourseApply.tsx
+++ src/container/Course/CourseApply.tsx
@@ -2,11 +2,12 @@
  * @Author: yyao
  * @Date: 2020-04-26 10:55:19
  * @LastEditors: yyao
- * @LastEditTime: 2020-04-26 11:28:26
+ * @LastEditTime: 2020-04-26 12:06:56
  * @Description: 课程报选
  */
 import React from "react";
-import { Tabs } from "antd";
+import { List, Avatar, Tabs } from "antd";
+import { MessageOutlined, LikeOutlined, StarOutlined } from "@ant-design/icons";
 import { StickyContainer, Sticky } from "react-sticky";
 import { BaseContainer } from "~/superClass/BaseContainer";
 
@@ -25,16 +26,93 @@ const renderTabBar = (props, DefaultTabBar) => (
 );
 
 @BaseContainer({})
-class CourseApply extends React.PureComponent {
+class CourseApply extends React.PureComponent<any, any> {
+  _renderList = () => {
+    const listData = [];
+    for (let i = 0; i < 23; i++) {
+      listData.push({
+        href: "http://ant.design",
+        title: `ant design part ${i}`,
+        avatar:
+          "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+        description:
+          "Ant Design, a design language for background applications, is refined by Ant UED Team.",
+        content:
+          "We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.",
+      });
+    }
+
+    const IconText = ({ icon, text }) => (
+      <span>
+        {React.createElement(icon, { style: { marginRight: 8 } })}
+        {text}
+      </span>
+    );
+
+    return (
+      <List
+        itemLayout="vertical"
+        size="large"
+        pagination={{
+          onChange: (page) => {
+            console.log(page);
+          },
+          pageSize: 3,
+        }}
+        dataSource={listData}
+        footer={
+          <div>
+            <b>ant design</b> footer part
+          </div>
+        }
+        renderItem={(item) => (
+          <List.Item
+            key={item.title}
+            actions={[
+              <IconText
+                icon={StarOutlined}
+                text="156"
+                key="list-vertical-star-o"
+              />,
+              <IconText
+                icon={LikeOutlined}
+                text="156"
+                key="list-vertical-like-o"
+              />,
+              <IconText
+                icon={MessageOutlined}
+                text="2"
+                key="list-vertical-message"
+              />,
+            ]}
+            extra={
+              <img
+                width={272}
+                alt="logo"
+                src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+              />
+            }
+          >
+            <List.Item.Meta
+              avatar={<Avatar src={item.avatar} />}
+              title={<a href={item.href}>{item.title}</a>}
+              description={item.description}
+            />
+            {item.content}
+          </List.Item>
+        )}
+      />
+    );
+  };
   render() {
     return (
       <StickyContainer>
         <Tabs defaultActiveKey="1" renderTabBar={renderTabBar}>
           <TabPane tab="可选课程" key="1" style={{ height: 200 }}>
-            Content of Tab Pane 1
+            {this._renderList()}
           </TabPane>
           <TabPane tab="已选课程" key="2">
-            Content of Tab Pane 2
+            {this._renderList()}
           </TabPane>
         </Tabs>
       </StickyContainer>
