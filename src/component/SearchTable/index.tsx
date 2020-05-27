9c43e4cfe3a5ad6aadee7a96cb0a177e1097e370
yangyao <873242789@qq.com>
2020-05-27 21:15:25 +0800

    优化table组件

--- src/component/SearchTable/index.tsx
+++ src/component/SearchTable/index.tsx
@@ -21,8 +21,6 @@ const columns = [
                 value: '',
             },
         ],
-        // specify the condition of filtering result
-        // here is that finding the name started with `value`
         onFilter: (value, record) => record.name.indexOf(value) === 0,
         sorter: (a, b) => a.name.length - b.name.length,
         sortDirections: ['descend'],
